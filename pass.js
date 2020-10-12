c3b197446180d2bee9a9e4d06b28a4081d807f35
ikmokhirio <ikmokhirio@mail.ru>
2020-10-12 18:57:12 +0300

    Added register strategy

--- pass.js
+++ pass.js
@@ -2,6 +2,7 @@ const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const CookieStrategy = require('passport-cookie').Strategy
 const User = require('./UserModel').User
+const createNewUser = require('./UserModel').createNewUser;
 const isUserExist = require('./database').isUserExist;
 const getUserPassword = require('./database').getUserPassword;
 const findUser = require('./database').findUser
@@ -43,6 +44,29 @@ const loginStrategy = new LocalStrategy({
     }
 );
 
+const registerStrategy = new LocalStrategy({
+        usernameField: 'username',
+        passwordField: 'password',
+        passReqToCallback: true
+    },
+    function (req, username, password, done) {
+
+        isUserExist(username).then(res => {
+            if (res) {
+                return done(null, false, {message: "User already exist"});
+            } else {
+                createNewUser(username, password, req.body.phone_number, req.body.gender, req.body.description).then(function (newUser) {
+                    return done(null, newUser);
+                }).catch(e => {
+                    return done(null, false, {message: "Error : " + e});
+                });
+            }
+        }).catch(e => {
+            return done(null, false, {message: "Error : " + e});
+        });
+    }
+);
+
 passport.serializeUser(function (user, done) {
     done(null, user.username);
 });
@@ -56,6 +80,7 @@ passport.deserializeUser(function (username, done) {
 });
 
 passport.use('login', loginStrategy);
+passport.use('register', registerStrategy);
 
 
 exports.passport = passport;
